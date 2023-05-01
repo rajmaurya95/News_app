85e8a6300cba960048e847f7d5b5f116d9540d44
Raj <gautamshakya9528@gmail.com>
2023-05-01 10:09:50 +0530

    Reinitialization of git

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News';
-import PropTypes from 'prop-types';
 import {
-  BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
-  Link,
-  useRouteMatch,
-  useParams
 } from "react-router-dom";
 
 
@@ -19,25 +14,17 @@ export default class App extends Component {
     return (
       <>
         <Navbar />
-        <Router>
-          <Switch>
-            <Route path="/business">
-              <News pageSize={6} country={"in"} topic={"top-headlines"} category={"business"} /></Route>
-            <Route path="/entertainment">
-              <News pageSize={6} country={"in"} topic={"top-headlines"} category={"entertainment"} /></Route>
-            <Route path="/general">
-              <News pageSize={6} country={"in"} topic={"top-headlines"} category={"general"} /></Route>
-            <Route path="/health">
-              <News pageSize={6} country={"in"} topic={"top-headlines"} category={"health"} /></Route>
-            <Route path="/science">
-              <News pageSize={6} country={"in"} topic={"top-headlines"} category={"science"} /></Route>
-            <Route path="/sports">
-              <News pageSize={6} country={"in"} topic={"top-headlines"} category={"sports"} /></Route>
-            <Route path="/technology">
-              <News pageSize={6} country={"in"} topic={"top-headlines"} category={"technology"} /></Route>
+        <Routes>
+          <Route exact path='/' element={<News key="general" pageSize={6} country={"in"} topic={"top-headlines"} category={"genral"} />}></Route>
+          <Route exact path="/business" element={<News key="business" pageSize={6} country={"in"} topic={"top-headlines"} category={"business"} />} ></Route>
+          <Route exact path="/entertainment" element={<News key="entertainment" pageSize={6} country={"in"} topic={"top-headlines"} category={"entertainment"} />} ></Route>
+          <Route exact path="/general" element={<News key="general" pageSize={6} country={"in"} topic={"top-headlines"} category={"general"} />} ></Route>
+          <Route exact path="/health" element={<News key="health" pageSize={6} country={"in"} topic={"top-headlines"} category={"health"} />} ></Route>
+          <Route exact path="/science" element={<News key="science" pageSize={6} country={"in"} topic={"top-headlines"} category={"science"} />} ></Route>
+          <Route exact path="/sports" element={<News key="sports" pageSize={6} country={"in"} topic={"top-headlines"} category={"sports"} />} ></Route>
+          <Route exact path="/technology" element={<News key="technology" pageSize={6} country={"in"} topic={"top-headlines"} category={"technology"} />} ></Route>
 
-          </Switch>
-        </Router>
+        </Routes>
 
       </>
     )
